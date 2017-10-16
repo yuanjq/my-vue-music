0456bdf85f97c8389ea0416bac2388d58da1437d
unknown <sky067785@skyworth.com>
2017-10-16 18:10:27 +0800

    update

--- src/components/recommend.vue
+++ src/components/recommend.vue
@@ -2,14 +2,30 @@
 	<div class="recommend" ref="recommend">
 		<scroll ref="scroll" class="recommend-content" :data="discList">
 			<div>
-				<div v-if="recommends.length" class="slider-wrapper" ref="sliderWrapper">
-					<slider>
-						<div v-for="item in recommends">
-							<a :href="item.linkUrl">
-								<img class="needsclick" @load="loadImage" :src="item.picUrl">
-							</a>
-						</div>
-					</slider>
+				<div v-if="recommends.length" class="slider-wrapper">
+					<div class="slider-content">
+						<slider ref="slider">
+							<div v-for="item in recommends">
+								<a :href="item.linkUrl">
+									<img @load="loadImage" :src="item.picUrl">
+								</a>
+							</div>
+						</slider>
+					</div>
+				</div>
+				<div class="recommend-list">
+					<h1 class="list-title">热门歌单推荐</h1>
+					<ul>
+						<li @click="selectItem(item)" v-for="item in discList" class="item">
+							<div class="icon">
+								<img width="60" height="60" v-lazy="item.imgurl">
+							</div>
+							<div class="text">
+								<h2 class="name" v-html="item.creator.name"></h2>
+								<p class="desc" v-html="item.dissname"></p>
+							</div>
+						</li>
+					</ul>
 				</div>
 			</div>
 			<div class="loading-container" v-show="!discList.length">
@@ -25,6 +41,7 @@
 	import Scroll from 'base/scroll'
 	import {getRecommend, getDiscList} from 'api/recommend'
 	import {ERR_OK} from 'api/config'
+	import {mapMutations} from 'vuex'
 
 	export default {
 		data() {
@@ -44,6 +61,12 @@
 	  			this.$refs.scroll.refresh()
 	  	  }			
 	    },
+	    selectItem(item) {
+	    	this.$router.push({
+	    		path: `/recommend/${item.dissid}`
+	    	})
+	    	this.setDisc(item)
+	    },
 	    _getRecommend() {
 	    	getRecommend().then((res) => {
 	    		if (res.code === ERR_OK) {
@@ -57,7 +80,10 @@
 	    			this.discList = res.data.list
 	    		}
 	    	})
-	    }
+	    },
+	    ...mapMutations({
+	    	setDisc: 'SET_DISC'
+	    })
 	  },
 	  components: {
 	  	Loading,
@@ -82,7 +108,35 @@
 				position: relative
 				width: 100%
 				overflow: hidden
-			
+			.recommend-list
+				.list-title
+					height: 65px
+					line-height: 65px
+					text-align: center
+					font-size: $font-size-medium
+					color: $color-theme
+				.item
+					display: flex
+					box-sizeing: border-box
+					align-items: center
+					padding: 0 20px 20px 20px
+					.icon
+						flex: 0 0 60px
+						width: 60px
+						padding-right: 20px
+					.text
+						display: flex
+						flex-direction: column
+						justify-content: center
+						flex: 1
+						line-height: 20px
+						overflow: hidden
+						font-size: $font-size-medium
+						.name
+							margin-bottom: 10px
+							color: $color-text
+						.desc
+							color: $color-text-d
 			.loading-container
 				position: absolute
 				width: 100%
